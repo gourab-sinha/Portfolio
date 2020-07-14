7837e55763e0e09628922597c8c7cee296725e63
gourab-sinha <gourab19964u@gmail.com>
2020-07-15 02:57:51 +0530

    New static file added in workExperience app and registered in settings.py

--- Portfolio/Portfolio/settings.py
+++ Portfolio/Portfolio/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ''
+SECRET_KEY = '6!1x%9@l+jinh$o+%$lfl&h!5)gxv86oplzr$%v^(d)$j9l_iy'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -124,4 +124,5 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
+    os.path.join(BASE_DIR, "workExperience/static")
 ]

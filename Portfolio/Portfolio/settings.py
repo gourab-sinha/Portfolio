fbb42ad3688704f303f5b1e00dd6305ec0470913
gourab-sinha <gourab19964u@gmail.com>
2020-07-14 17:22:18 +0530

    static directories path added

--- Portfolio/Portfolio/settings.py
+++ Portfolio/Portfolio/settings.py
@@ -56,6 +56,7 @@ TEMPLATES = [
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
             os.path.join(BASE_DIR, 'home/templates'),
+            os.path.join(BASE_DIR, 'workExperience/templates'),
             os.path.join(BASE_DIR, 'templates')
         ],
         'APP_DIRS': True,
@@ -121,3 +122,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
